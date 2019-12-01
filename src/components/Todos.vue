4ea3de09becbc5ef0354ee297895bdc61844815f
waspryo <wasp2123321@gmail.com>
2019-12-01 16:59:31 +0900

    create files

---
+++ src/components/Todos.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>Todos</h3>
+</template>
+
+<script>
+export default {
+  name: "Todos"
+};
+</script>
+
+<style>
+</style>
---
+++ src/store/modules/todos.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Todo one'
+        },
+        {
+            id: 2,
+            title: 'Todo two'
+        }
+    ]
+};
+const getters = {};
+const actions = {};
+const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
