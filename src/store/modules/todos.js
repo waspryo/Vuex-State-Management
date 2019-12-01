8c0bb73a5a58f0109abe987be4e3bf7804b23a73
waspryo <wasp2123321@gmail.com>
2019-12-01 17:08:52 +0900

    get the data

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,24 +1,26 @@
-import axios from 'axios'
+// import axios from "axios";
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo one'
-        },
-        {
-            id: 2,
-            title: 'Todo two'
-        }
-    ]
+  todos: [
+    {
+      id: 1,
+      title: "Todo one"
+    },
+    {
+      id: 2,
+      title: "Todo two"
+    }
+  ]
 };
-const getters = {};
+const getters = {
+  allTodos: state => state.todos
+};
 const actions = {};
 const mutations = {};
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
-}
+  state,
+  getters,
+  actions,
+  mutations
+};
